1d712bfdbff6fd7cb48431d4665420602af2e8a5
hang <hangyuan@umich.edu>
2018-01-11 20:31:14 -0500

    s1

---
+++ s1.cpp
@@ -0,0 +1,22 @@
+#include <vector> 
+#include <algorithm>
+#include <functional>
+#include <iostream>
+
+using namespace std;
+
+int main (int argc, char** argv) {
+
+	int ia[6] = {27, 210, 12, 35, 109, 85};
+
+	vector<int, allocator<int>> vi(ia, ia+6);
+
+	cout << count_if(vi.begin(), vi.end(), 
+		not1(bind2nd(less<int>(), 40))) << endl;
+
+	auto foo = not2(less<int>());
+	if (foo(5,4))
+		cout << "A" << endl;
+
+	return 0;
+}
